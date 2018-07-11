16dff6f47af35d6c93b29268e4d9a96bd3aeb474
Justin van Zanten <justinvzantenit@gmail.com>
2018-07-12 00:21:33 +0200

    Initial commit

---
+++ src/main/java/nl/jvz/example/springapp/Application.java
@@ -0,0 +1,29 @@
+package nl.jvz.example.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    String indexPage(final Model model) {
+        return "Hello world";
+    }
+}
